9859f851ddb3962f81fa3dae550a60735a028749
Luma Chen <lumadafonseca@gmail.com>
2021-03-10 23:53:12 -0300

    Melhorando o código e adicionando botões

--- minha_dieta/src/App.js
+++ minha_dieta/src/App.js
@@ -1,98 +1,117 @@
-import './App.css';
-import React from 'react';
-import Table from './components/Table';
-import NavBar from '../src/components/NavBar';
-import Carrousel from './components/Carrousel';
-import NavBarSearchBox from './components/NavBarSearchBox';
-
+import "./App.css";
+import React, { useState } from "react";
+import Table from "./components/Table";
+import NavBar from "../src/components/NavBar";
+import Carrousel from "./components/Carrousel";
+import NavBarSearchBox from "./components/NavBarSearchBox";
 
 const categoria_dietas = [
-  {nome: 'MONTAR PRATO'},
-  {nome: 'CETOGÊNICA'},
-  {nome: 'DASH'},
-  {nome: 'DUKAN'},
-  {nome: 'GLUTEN FREE'},
-  {nome: 'LOW-CARB'},
-  {nome: 'MEDITERRÂNEA'},
-  {nome: 'PALEOLÍTICA'},
-  {nome: 'RAVENNA'},
-  {nome: 'SEM LACTOSE'},
-  {nome: 'VEGANA'}
-]
+  { nome: "MONTAR PRATO" },
+  { nome: "CETOGÊNICA" },
+  { nome: "DASH" },
+  { nome: "DUKAN" },
+  { nome: "GLUTEN FREE" },
+  { nome: "LOW-CARB" },
+  { nome: "MEDITERRÂNEA" },
+  { nome: "PALEOLÍTICA" },
+  { nome: "RAVENNA" },
+  { nome: "SEM LACTOSE" },
+  { nome: "VEGANA" },
+];
 
 const categoria_alimentos = [
-  {nome: 'CARNES E OVOS'},
-  {nome: 'CEREAIS'},
-  {nome: 'FRUTAS'},
-  {nome: 'LEGUMES'},
-  {nome: 'LEGUMINOSAS'},
-  {nome: 'ÓLEOS E GORDURAS'},
-  {nome: 'PÃES'},
-  {nome: 'TUBÉRCULOS'},
-  {nome: 'VERDURAS'},
-  {nome: 'OUTROS'}
-]
+  { nome: "CARNES E OVOS" },
+  { nome: "CEREAIS" },
+  { nome: "FRUTAS" },
+  { nome: "LEGUMES" },
+  { nome: "LEGUMINOSAS" },
+  { nome: "ÓLEOS E GORDURAS" },
+  { nome: "PÃES" },
+  { nome: "TUBÉRCULOS" },
+  { nome: "VERDURAS" },
+  { nome: "OUTROS" },
+];
 
 const categoria_bebidas = [
-  {nome: 'CHÁS'},
-  {nome: 'LATICÍNIOS'},
-  {nome: 'SUCOS'},
-  {nome: 'OUTROS'}
-]
+  { nome: "CHÁS" },
+  { nome: "LATICÍNIOS" },
+  { nome: "SUCOS" },
+  { nome: "OUTROS" },
+];
 
-const dieta= [
-  {nome: 'Suco de Laranja', quantidade: 100, caloria: 300},
-  {nome: 'Alface', quantidade: 100, caloria: 200},
-  {nome: 'Banana', quantidade: 100, caloria: 500},
-  {nome: 'Alcatra', quantidade: 100, caloria: 700},
-  {nome: 'Queijo', quantidade: 100, caloria: 400},
-  
-]
+const dieta = [
+  { nome: "Refrigerante", quantidade: 100, caloria: 300 },
+  { nome: "Alface", quantidade: 100, caloria: 200 },
+  { nome: "Banana", quantidade: 100, caloria: 500 },
+  { nome: "Alcatra", quantidade: 100, caloria: 700 },
+  { nome: "Queijo", quantidade: 100, caloria: 400 },
+];
 
-const alimentos= [
-  {nome: 'Alcatra', quantidade: 100, caloria: 300},
-  {nome: 'Alface', quantidade: 100, caloria: 200},
-  {nome: 'Banana', quantidade: 100, caloria: 500},
-  {nome: 'Goiaba', quantidade: 100, caloria: 700},
-  {nome: 'Queijo', quantidade: 100, caloria: 400},
-]
+const alimentos = [
+  { nome: "Alcatra", quantidade: 100, caloria: 300 },
+  { nome: "Alface", quantidade: 100, caloria: 200 },
+  { nome: "Banana", quantidade: 100, caloria: 500 },
+  { nome: "Goiaba", quantidade: 100, caloria: 700 },
+  { nome: "Queijo", quantidade: 100, caloria: 400 },
+];
 
-const bebidas= [
-  {nome: 'Guaraná', quantidade: 100, caloria: 300},
-  {nome: 'Chá de hibisco', quantidade: 100, caloria: 200},
-  {nome: 'Café', quantidade: 100, caloria: 500},
-  {nome: 'Suco de Uva', quantidade: 100, caloria: 700},
-  {nome: 'Suco de Laranja', quantidade: 100, caloria: 400},
-]
+const bebidas = [
+  { nome: "Guaraná", quantidade: 100, caloria: 300 },
+  { nome: "Chá de hibisco", quantidade: 100, caloria: 200 },
+  { nome: "Café", quantidade: 100, caloria: 500 },
+  { nome: "Suco de Uva", quantidade: 100, caloria: 700 },
+  { nome: "Refrigerante", quantidade: 100, caloria: 400 },
+];
 
-function App (props) 
-{
-  return (<>
-    <NavBar></NavBar>
-    <NavBarSearchBox></NavBarSearchBox>
-    <Carrousel></Carrousel>
-    <div class="container" >
-            <div class="row">
-                
-                <div class="col">
-                    <span id="fonte1">Dieta</span>
-                    <div class="input-group"><input  type="text" id="txtBusca" placeholder="Buscar Dieta..."/></div>
-                    <Table alimentos= {dieta} />
-                </div>
-                <div class="col">
-                    <span id="fonte1">Alimentos</span>
-                    <div class="input-group"><input type="text" id="txtBusca" placeholder="Buscar Alimento..."/></div>
-                    <Table alimentos= {alimentos} />
-                </div>
-                <div class="col">
-                    <span id="fonte1">Bebidas</span>
-                    <div class="input-group"><input  type="text" id="txtBusca" placeholder="Buscar Bebida..."/></div>
-                    <Table alimentos= {bebidas} />
-                </div>
+function App(props) {
+  // const [prato, setPrato] = useState([]);
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <NavBarSearchBox></NavBarSearchBox>
+      <Carrousel></Carrousel>
+      <div class="container">
+        <div class="row">
+          <div>
+            {" "}
+            <br />
+            <b>
+              Seja bem-vindo! Para começar escolha uma dieta ou selecione
+              alimentos/bebida:
+            </b>
+          </div>
+          <div class="col">
+            {" "}
+            <br />
+            <span id="fonte1">Dieta</span>
+            <div class="input-group">
+              <input type="text" id="txtBusca" placeholder="Buscar Dieta..." />
             </div>
-    </div>       
-    
-  </>);
+            <Table alimentos={dieta} />
+          </div>
+          <div class="col">
+            <span id="fonte1">Alimentos</span>
+            <div class="input-group">
+              <input
+                type="text"
+                id="txtBusca"
+                placeholder="Buscar Alimento..."
+              />
+            </div>
+            <Table alimentos={alimentos} />
+          </div>
+          <div class="col">
+            <span id="fonte1">Bebidas</span>
+            <div class="input-group">
+              <input type="text" id="txtBusca" placeholder="Buscar bebida..." />
+            </div>
+            <Table alimentos={bebidas} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- minha_dieta/src/components/Table.js
+++ minha_dieta/src/components/Table.js
@@ -1,34 +1,53 @@
-import React from 'react';
-import img5 from '../../src/images/buttons/menos1.jpg';
+import React from "react";
+import img5 from "../../src/images/buttons/menos1.jpg";
+import "./Table.css";
 
-
-const Tabela = (props) => {
-    return (
-        <tr>
-            <td>
-                <a href="" ><img id="imgplus1" src={img5}></img></a>
-                {props.alimento.nome}
-            </td>
-            <td>
-                {props.alimento.quantidade}
-            </td>
-            <td>
-                {props.alimento.caloria}
-            </td>
-        </tr>
-    )
+function adicionar()
+{
+    alert("Deseja adicionar este item a sua dieta?");
+    /* const itensCopy = Array.from(prato);
+    itensCopy.push({nome: prato})
+    setPrato(itensCopy); */
 }
 
+const Tabela = (props) => {
+  return (
+    <tr>
+      <td>
+        <button id="botaoAdd" onClick= {() => adicionar()}>
+          <img id="imgplus1" src={img5}></img>
+        </button>
+        {props.alimento.nome}
+      </td>
+      <td>{props.alimento.quantidade}</td>
+      <td>{props.alimento.caloria}</td>
+    </tr>
+  );
+};
 
-export default function Tables (props){
-    return(
-        <table id="minhaTabela" >
-            <thead><tr id="title"><th>COMPONENTE</th><th>QTD</th><th>Kcal</th></tr></thead>
-            <tbody>
-                {props.alimentos.map((alimento) => <Tabela key= {alimento.nome} alimento= {alimento} /> )}
-            </tbody>
-            <tfoot><tr> <th> </th><th></th><th></th></tr></tfoot>
-        </table>
-
-    )
+export default function Tables(props) {
+  return (
+    <table id="minhaTabela">
+      <thead>
+        <tr id="title">
+          <th>COMPONENTE</th>
+          <th>QTD</th>
+          <th>Kcal</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.alimentos.map((alimento) => (
+          <Tabela key={alimento.nome} alimento={alimento} />
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          {" "}
+          <th> </th>
+          <th></th>
+          <th></th>
+        </tr>
+      </tfoot>
+    </table>
+  );
 }
