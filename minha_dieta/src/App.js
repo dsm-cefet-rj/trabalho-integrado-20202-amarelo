873e42b536a49438e8b2c2948f57c76d75612f5f
Luma Chen <lumadafonseca@gmail.com>
2021-03-14 21:04:04 -0300

    Reducer criado

--- minha_dieta/src/App.js
+++ minha_dieta/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, Provider } from "react";
 import Table from "./components/Table";
 import NavBar from "../src/components/NavBar";
 import Carrousel from "./components/Carrousel";
 import NavBarSearchBox from "./components/NavBarSearchBox";
-import AddButton from './components/Button';
-import InputBox from './components/InputBox';
+import AddButton from "./components/Button";
+import InputBox from "./components/InputBox";
 import img5 from "./images/buttons/menos1.png";
 import img6 from "./images/buttons/mais1.png";
 
@@ -67,9 +67,7 @@ const bebidas = [
   { nome: "Refrigerante", quantidade: 100, caloria: 400 },
 ];
 
-const prato = [
-
-]
+const prato = [];
 
 function App(props) {
   // const [prato, setPrato] = useState([]);
@@ -79,25 +77,48 @@ function App(props) {
       <NavBar></NavBar>
       <NavBarSearchBox></NavBarSearchBox>
       <Carrousel></Carrousel>
-      <br/>
+      <br />
       <div class="container">
         <div class="row">
-        <div class="container"><p class="textobv"> Seja bem-vindo! Para começar escolha uma dieta ou selecione alimentos/bebida: </p> </div>
+          <div class="container">
+            <p class="textobv">
+              {" "}
+              Seja bem-vindo! Para começar escolha uma dieta ou selecione
+              alimentos/bebida:{" "}
+            </p>{" "}
+          </div>
           <div class="col">
             <br />
-            <span id="fonte1">Dieta  <AddButton nome='Salvar' ></AddButton></span>
-            <InputBox nome='Buscar Dieta...'></InputBox>
-            <Table alimentos={dieta} url={img5} col1="COMPONENTE" rod1="TOTAL" rod2="600" rod3="1200" />
+            <span id="fonte1">
+              Dieta <AddButton nome="Salvar"></AddButton>
+            </span>
+            <InputBox nome="Buscar Dieta..."></InputBox>
+            <Table
+              alimentos={dieta}
+              url={img5}
+              col1="COMPONENTE"
+              rod1="TOTAL"
+              rod2="600"
+              rod3="1200"
+            />
           </div>
-          <div class="col"> <br />
-            <span id="fonte1">Alimentos  <AddButton nome='Cadastrar'></AddButton></span>
-            <InputBox nome='Buscar Alimento...'></InputBox>
-            <Table alimentos={alimentos} url={img6} col1="ALIMENTO"/>
+          <div class="col">
+            {" "}
+            <br />
+            <span id="fonte1">
+              Alimentos <AddButton nome="Cadastrar"></AddButton>
+            </span>
+            <InputBox nome="Buscar Alimento..."></InputBox>
+            <Table alimentos={alimentos} url={img6} col1="ALIMENTO" />
           </div>
-          <div class="col"> <br />
-            <span id="fonte1">Bebidas  <AddButton nome='Cadastrar'></AddButton></span>
-            <InputBox nome='Buscar Bebida...'></InputBox>
-            <Table alimentos={bebidas} url={img6} col1="BEBIDA"/>
+          <div class="col">
+            {" "}
+            <br />
+            <span id="fonte1">
+              Bebidas <AddButton nome="Cadastrar"></AddButton>
+            </span>
+            <InputBox nome="Buscar Bebida..."></InputBox>
+            <Table alimentos={bebidas} url={img6} col1="BEBIDA" />
           </div>
         </div>
       </div>
--- minha_dieta/src/index.js
+++ minha_dieta/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+import rootReducer from './redux/reducer';
 //import reportWebVitals from './reportWebVitals';
 //import * as serviceWorker from './serviceWorker';
 
+const store= createStore(rootReducer);
+
 ReactDOM.render(
   <App  />, document.getElementById('root')
-  
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
