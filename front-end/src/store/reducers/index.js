f0801f7145b683c85c51540337082fb28f2e18ed
Luma Chen <41763736+Luma-Chen@users.noreply.github.com>
2021-04-03 15:24:14 -0300

    Delete front-end/src directory

--- front-end/src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import pratos from './pratosReducer';
-
-export default combineReducers({
-    pratos,
-})
--- front-end/src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from './reducers/index';
-
-export const store = configureStore({
-  reducer: reducer,
-});
